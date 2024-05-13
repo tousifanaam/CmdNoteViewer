dde058a96d5a5e30f92ddcc6a4743de5b07661d0
tousifanaam <64709584+tousifanaam@users.noreply.github.com>
2024-05-13 15:57:37 +0600

    Update view.py

--- view.py
+++ view.py
@@ -1,9 +1,9 @@
 from os import listdir, system
-from basics import Menu
+from basics import *
 from prettytable import PrettyTable
 
 
-# Define ANSI color escape codes
+# ANSI Colors
 COLOR_RED = '\033[91m'
 COLOR_GREEN = '\033[92m'
 COLOR_YELLOW = '\033[93m'
@@ -12,7 +12,7 @@ COLOR_CYAN = '\033[96m'
 COLOR_WHITE_BG = "\033[47m"
 COLOR_LIGHT_GRAY_BG = "\033[47;30m"
 COLOR_CLEAR_SCREEN = "\033[2J"
-COLOR_RESET = '\033[0m'  # Reset to default color
+COLOR_RESET = '\033[0m'
 
 def fileview(filename):
 
@@ -27,27 +27,33 @@ def fileview(filename):
             table0.add_row(load)
         print(table0)
 
-    _ = system('cls')  # Clear screen (Windows)
+    clear() # clear screen
     with open(filename, "r+") as file:
         paragraph_in: bool = False
         table_data = []
         in_table: bool = False
         for line in file.readlines():
-            if line.startswith("<title>"):
+            if line.startswith("<title>"): # main title
                 title = "TITLE: " + line[7:].upper().strip()
                 print(f"\n{f'{COLOR_WHITE_BG}{COLOR_RED}Bismillahir Rahmanir Rahim{COLOR_RESET}'.center(100)}\n")
                 print(f"{COLOR_BLUE}\n{title.center(100)}{COLOR_RESET}")
                 print("-" * 100 + "\n\n")
-            elif line.startswith("<t>"):
+            elif line.startswith("<t>"): # topic
                 print(f"{COLOR_YELLOW}{' '.join(line[3:]).center(100)}{COLOR_RESET}\n")
-            elif line.strip().startswith("-"):
+            elif line.strip().startswith("-"): # bullet point
                 bullet = line[:line.index('-')] + "• " + line[line.index('-') + 1:]
                 print(f"{COLOR_GREEN}{bullet}{COLOR_RESET}")
-            elif line.startswith("<i>"):
+            elif line.startswith("<i>"): # important
                 print(f"{COLOR_RED}[ ♠ ] {line[3:]}{COLOR_RESET}")
-            elif line.startswith("<!p>"):
+            elif line.startswith("<e>"): # empty line
+                print("\n")
+            elif line.startswith("<h>"): # highlight
+                print(f"{COLOR_LIGHT_GRAY_BG}{COLOR_BLUE} {COLOR_RED}{line[3:-1].strip()}{COLOR_RESET}")
+            elif line.startswith("<w>"): # warning
+                print(f"{COLOR_GREEN}{COLOR_YELLOW}  ⚠  {COLOR_GREEN} {COLOR_RED}{line[3:].strip()}{COLOR_RESET}")
+            elif line.startswith("<!p>"): # pragrahph end
                 paragraph_in = False
-            elif line.startswith("<p>") or paragraph_in:
+            elif line.startswith("<p>") or paragraph_in: # paragraph start
                 paragraph_in = True
                 if line[3:].strip() == "":
                     continue
@@ -55,19 +61,17 @@ def fileview(filename):
                     print(line[3:])
                 else:
                     print(line)
-            elif line.startswith("<!ta>"):
+            elif line.startswith("<!ta>"): # table end
                 table_print(table_data)
                 in_table = False
                 table_data = []
-            elif line.startswith("<ta>"):
+            elif line.startswith("<ta>"): # table start
                 in_table = True
             elif in_table:
                 table_data.append([i.strip() for i in line.split(",")])
-            elif line.startswith("<e>"):
-                print("\n")
             else:
                 pass
-    print("-" * 100 + "\n\n")
+    print("\n" + "-" * 100 + "\n\n")
     print(f"\n{f'{COLOR_WHITE_BG}{COLOR_RED}Alhamdulillah{COLOR_RESET}'.center(100)}\n")
 
 
